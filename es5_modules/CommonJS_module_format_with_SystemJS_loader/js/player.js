7dc928cd2ca462f9b00d403a1e13f14cb710da45
kumudug <kumudug1@gmail.com>
2016-11-26 09:37:51 +1100

    CommonJS module format with SystemJS loader

---
+++ es5_modules/CommonJS_module_format_with_SystemJS_loader/js/player.js
@@ -0,0 +1,30 @@
+//var player = function() {
+
+// private members
+var playerName = '';
+
+function logPlayer() {
+    console.log('The current player is ' + playerName + '.');
+}
+
+function setName(newName) {
+    playerName = newName;
+}
+
+function getName() {
+    return playerName;
+}
+
+// return {
+//     logPlayer: logPlayer,
+//     setName: setName,
+//     getName: getName
+// };
+
+module.exports = {
+    logPlayer: logPlayer,
+    setName: setName,
+    getName: getName
+}
+
+//}();
